33e589df8bdb9dbe80b6e2949e12726ba2688cbb
anuja <anujaadhikari24@gmail.com>
2026-08-30 18:20:47 +0545

    navbar added

--- hotel-booking/src/components/Navbar.jsx
+++ hotel-booking/src/components/Navbar.jsx
@@ -1,22 +1,36 @@
 import { Heart, Menu, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="container nav-content">
-        <div className="logo">
+
+        {/* LOGO */}
+        <Link to="/" className="logo">
           <span>Stay</span>Ease
-        </div>
+        </Link>
 
+        {/* NAVIGATION */}
         <div className="nav-links">
-          <a href="#home">Home</a>
-          <a href="#hotels">Hotels</a>
-          <a href="#offers">Offers</a>
-          <a href="#about">About</a>
-          <a href="#contact">Contact</a>
+          <Link to="/">Home</Link>
+
+          <Link to="/hotels">Hotels</Link>
+
+          <Link to="/offers">Offers</Link>
+
+          <Link to="/about">About</Link>
+
+          <Link to="/contact">Contact</Link>
+
+          <Link to="/my-bookings">
+            My Bookings
+          </Link>
         </div>
 
+        {/* ACTIONS */}
         <div className="nav-actions">
+
           <button className="icon-button">
             <Heart size={20} />
           </button>
@@ -25,11 +39,14 @@ function Navbar() {
             <User size={18} />
             Login
           </button>
+
         </div>
 
+        {/* MOBILE MENU */}
         <button className="menu-button">
           <Menu size={24} />
         </button>
+
       </div>
     </nav>
   );
