daa2927aacbca3705939a2c6354cc1e9dd687f23
anuja <anujaadhikari24@gmail.com>
2026-09-12 20:33:59 +0545

    Adding server

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "StayEase Backend is running",
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
